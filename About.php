ff1e8b3e9b195d6a2cf0b3dfa8765d630c72c569
Krauss <merlosamuel@protonmail.ch>
2023-02-26 13:17:29 -0300

    Update: New UI and Features
    Update: - Ícones de Add e Menu
    - Nova UI
    
    Adiciona: - PHP para lidar com a nova task
    - View para adicionar task
    
    Remove: - Propriedade "Percentage" do obj Task.
    - Arquivos velhos desnecessários: Ícones, UI antiga, Páginas não usadas.
    
    Fix: Padrão MVC

--- About.php
+++
@@ -1,5 +0,0 @@
-<?php require_once("View/User/Header.php"); ?>
-
-<main></main>
-
-<?php require_once("View/User/Footer.php"); ?>
--- View/User/Footer.php
+++
@@ -1,7 +0,0 @@
-        <footer>
-            <p>Made By Samuel Melo</p>
-        </footer>
-
-        <script src="View/Js/main.js"></script>
-    </body>
-</html>
